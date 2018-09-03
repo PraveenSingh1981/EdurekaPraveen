06acc5bc9f0b89308db7fd3c465008303af03561
prave <prave@DESKTOP-76S09T1>
2018-09-03 23:36:12 +0530

    Adding the code for Variables/ Datatypes and Greatest of two numbers

---
+++ Java_Learning_Project/src/day2/GreatestOutOfTwoNumbers.java
@@ -0,0 +1,32 @@
+package day2;
+
+public class GreatestOutOfTwoNumbers {
+
+	public static void main(String[] args) {
+
+		int firstnum, secondnum;
+
+		firstnum = 90;
+		secondnum = 90;
+
+		if (firstnum > secondnum)
+
+		{
+			System.out.println("First Number is Greatest");
+		}
+
+		if (firstnum == secondnum)
+
+		{
+			System.out.println("Both Numbers are Equal");
+		}
+
+		else
+
+		{
+			System.out.println("Second Number is Greatest");
+		}
+
+	}
+
+}
---
+++ Java_Learning_Project/src/day2/VariableExamples.java
@@ -0,0 +1,25 @@
+package day2;
+
+public class VariableExamples {
+
+	public static void main(String[] args) {
+
+		int FirstNumber = 10;
+
+		int SecondNumber = 20;
+
+//		int Final = FirstNumber + SecondNumber;
+
+//		System.out.println("First Number :" + FirstNumber);
+//		System.out.println("First Number :" + FirstNumber + SecondNumber);
+//		System.out.println("First Number :" + (FirstNumber + SecondNumber));
+//		System.out.println("Final Number :" + (Final));
+		
+		System.out.println(FirstNumber);
+		System.out.println(FirstNumber++);
+		System.out.println(++FirstNumber);
+		
+			
+	}
+
+}
